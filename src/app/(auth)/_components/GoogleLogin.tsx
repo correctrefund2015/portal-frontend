a3d2924c7394326272cc22ea1b70a9baaaa735df
hamza <hamzagill415@gmail.com>
2024-10-25 07:31:33 -0400

    fixing the build issue

--- src/app/(auth)/_components/GoogleLogin.tsx
+++ src/app/(auth)/_components/GoogleLogin.tsx
@@ -1,37 +1,37 @@
-"use client";
-import { useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect } from "react";
+// "use client";
+// import { useRouter, useSearchParams } from "next/navigation";
+// import React, { useEffect } from "react";
 
-const GoogleLogin = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
+// const GoogleLogin = () => {
+//   const router = useRouter();
+//   const searchParams = useSearchParams();
 
-  const login = async (accessToken: string, refreshToken: string) => {
-    const resp = await fetch("/api/google-login", {
-      method: "GET",
-      body: JSON.stringify({ accessToken, refreshToken }),
-    });
-    console.log('in google login');
+//   const login = async (accessToken: string, refreshToken: string) => {
+//     const resp = await fetch("/api/google-login", {
+//       method: "GET",
+//       body: JSON.stringify({ accessToken, refreshToken }),
+//     });
+//     console.log('in google login');
 
-    return resp.ok ? resp.json() : null;
-  };
+//     return resp.ok ? resp.json() : null;
+//   };
 
-  useEffect(() => {
-    const accessToken = searchParams.get("redirect");
-    const refreshToken = searchParams.get("token");
-    if (accessToken && refreshToken) {
-      login(accessToken, refreshToken).then((data: any) => {
-        if (data.message === "ok") {
-          // console.log({ data });
+//   useEffect(() => {
+//     const accessToken = searchParams.get("redirect");
+//     const refreshToken = searchParams.get("token");
+//     if (accessToken && refreshToken) {
+//       login(accessToken, refreshToken).then((data: any) => {
+//         if (data.message === "ok") {
+//           // console.log({ data });
 
-          router.replace(data.route);
-          router.refresh();
-        }
-      });
-    }
-  }, [router, searchParams]);
+//           router.replace(data.route);
+//           router.refresh();
+//         }
+//       });
+//     }
+//   }, [router, searchParams]);
 
-  return <div>Processing authentication...</div>;
-};
+//   return <div>Processing authentication...</div>;
+// };
 
-export default GoogleLogin;
+// export default GoogleLogin;
